6eec7b869a8761505f9a303f40f940492a318b9c
setq <setq@radxa.com>
2023-12-22 20:35:50 +0800

    modify section func again
    Signed-off-by: setq <setq@radxa.com>

--- src/utils/docs.js
+++ src/utils/docs.js
@@ -1,7 +1,7 @@
-export const Section = ({compatible, model, children}) => {
+export const Section = ({ compatible, model, children }) => {
   const eq = (elem) => elem.trim() == model;
   if (!compatible.split(",").some(eq)) {
     children = "";
   }
-  return <>{children}</>
+  return <>{children}</>;
 };
