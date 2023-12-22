0b2566ed7f3515741c0ef900cc264df3b9fd5715
setq <setq@radxa.com>
2023-12-22 20:29:14 +0800

    modify section function
    Signed-off-by: setq <setq@radxa.com>

--- src/utils/docs.js
+++ src/utils/docs.js
@@ -1,7 +1,7 @@
 export const Section = ({compatible, model, children}) => {
   const eq = (elem) => elem.trim() == model;
-  if (!compatible.split(',').some(eq)) {
-    children = '';
+  if (!compatible.split(",").some(eq)) {
+    children = "";
   }
   return <>{children}</>
-}
+};
